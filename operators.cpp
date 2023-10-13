3c4721122bd3843d048e3383c26acbde4628fa4f
Bipin-Chourasia <144836846+Bipin-Chourasia@users.noreply.github.com>
2023-10-13 23:50:42 +0530

    Add files via upload

---
+++ operators.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a=7;
+	int b=6;
+	int c= (a>b);
+	int f=(a==b)||(a<b);
+	int e=(a&b);
+	printf("%d\n",c);
+	printf("%d\n",f);
+	printf("%d\n",e);
+}
