c05a38b20b60f66b9bf180569c9ddf64359746c7
Bipin-Chourasia <144836846+Bipin-Chourasia@users.noreply.github.com>
2023-10-20 22:48:07 +0530

    Add files via upload

---
+++ sum with beep sound.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter the value of a:\n\a");
+	scanf("%d",&a);
+	printf("enter the value of b:\n\a");
+	scanf("%d",&b);
+	c=a+b;
+	printf("%d\a\n",c);
+}
