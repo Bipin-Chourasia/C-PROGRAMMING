be971245f2a79a9e520dc7aaf5005815d9426e42
Bipin Chourasia <144836846+Bipin-Chourasia@users.noreply.github.com>
2023-10-30 23:45:48 +0530

    Add files via upload

---
+++ Biodata.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int c,d;
+	char b,a;
+	printf("Please name:-\n");
+	scanf("%c",&a);
+	printf("what is your father name?\n");
+	scanf("%c",&b);
+	printf("tell me your birthday year:-\n");
+	scanf("%d",&c);
+	printf("Tell me about your qualification:-\n");
+	
+}
