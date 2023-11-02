460addc044da4306da5ffa614eb39cc22bc7fcd7
Bipin Chourasia <144836846+Bipin-Chourasia@users.noreply.github.com>
2023-11-02 22:32:52 +0530

    Add files via upload

---
+++ Five bestfriends name by using function.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void displaybestfriend(){
+	printf("A\n");
+	printf("B\n");
+	printf("C\n");
+	printf("D\n");
+	printf("E\n");
+}
+int main(){
+	displaybestfriend();
+	return 0;
+}
