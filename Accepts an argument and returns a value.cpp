fcf2f09c6576361f34e6cd8f67af6af52a866c6b
Bipin Chourasia <144836846+Bipin-Chourasia@users.noreply.github.com>
2023-11-03 23:27:15 +0530

    Add files via upload

---
+++ Accepts an argument and returns a value.cpp
@@ -0,0 +1,13 @@
+//ACCEPTS AN ARGUMENT AND RETURNS A VALUE.
+#include<stdio.h>
+int add(int a,int ){
+	int c=a+b;
+	return c;
+}
+int main(){
+	int a=0; b=0;
+	printf("Enter the value of A and B:\n");
+	sacnf("%d %d", &a,&b);
+	int val=add(a,b)
+	printf("%d", val);
+}
