18b5aa2f235f7e5e62bf8b0eb8d136593baa5a28
Bipin Chourasia <144836846+Bipin-Chourasia@users.noreply.github.com>
2023-11-05 23:30:03 +0530

    Add files via upload

---
+++ Positive or negative.cpp
@@ -0,0 +1,13 @@
+// WRITE A PROGRAN IN C TO CHECK THE NUMBER IS POSITIVE OR NEGATIVE.
+#include<stdio.h>
+int main()
+{
+int a;
+printf("Enter the value of A:\n");
+scanf("%d",&a);
+if(a>0)
+printf("A is positive");
+else 
+printf("A is negative");
+}
+
