79e509329560c8b41ca43a880dcf37a518e20035
Bipin Chourasia <144836846+Bipin-Chourasia@users.noreply.github.com>
2023-11-10 23:40:39 +0530

    Add files via upload

---
+++ Length of a string.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int length = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    printf("Length of the string: %d\n", length);
+
+    return 0;
+}
+
