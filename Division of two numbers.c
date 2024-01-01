a94705a5b8c7a847cac48819c44c462ad771ac0a
Bipin-Chourasia <144836846+Bipin-Chourasia@users.noreply.github.com>
2024-01-01 17:58:56 +0530

    Division

---
+++ Division of two numbers.c
@@ -0,0 +1,11 @@
+// Write a progran in c to divide two numbers.
+#include<stdio.h>
+int main(){
+    float a,b,c;
+    printf("Enter the dividend:\n");
+    scanf("%f",&a);
+    printf("Enter the divisior:\n");
+    scanf("%f",&b);
+    c=a/b;
+    printf("Quotient is: %f",c);
+}
