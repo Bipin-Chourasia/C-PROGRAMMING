ccf3197e501f794911d1269da0d8a06be3e80241
Bipin-Chourasia <144836846+Bipin-Chourasia@users.noreply.github.com>
2024-01-04 23:20:14 +0530

    sequence 3

---
+++ Pattern sequence 3.c
@@ -0,0 +1,16 @@
+//Write a program in c to print the following pattern sequence
+**********
+**********
+**********
+**********
+**********
+#include<stdio.h>
+int main(){
+ int i,j;
+ for(i=1;i<=5;i++){
+     printf("\n");
+ for(j=1;j<=10;j++)
+     printf("*");
+}
+return 0;
+}
